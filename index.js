521583f2a5e53c9f274613a4d5e510614acb3158
Ayesha <ayeshamunawarofficial@gmail.com>
2021-02-19 21:34:06 +0500

    BU-B1> Add images ot the slick carousel and the caption to it
    BU-B1> Add images ot the slick carousel and the caption to it

--- index.js
+++ index.js
@@ -28,3 +28,96 @@ $('.one-time').slick({
         }
     ]
 });
+
+$('.slick-center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    speed: 300,
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                arrows: true,
+                centerMode: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+
+$(".slick-vertical-center-4").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+    slidesToShow: 4,
+    slidesToScroll: 2
+});
+$(".slick-vertical-center-3").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
+$(".slick-vertical-center-2").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+    slidesToShow: 2,
+    slidesToScroll: 2
+});
+$(".slick-vertical-center").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+});
+$(".slick-vertical").slick({
+    dots: true,
+    vertical: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
+$(".slick-regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
+$(".slick-center").slick({
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 3
+});
+$(".slick-variable").slick({
+    dots: true,
+    infinite: true,
+    variableWidth: true
+});
+$(".slick-lazy").slick({
+    lazyLoad: 'ondemand', // ondemand progressive anticipated
+    infinite: true
+});
